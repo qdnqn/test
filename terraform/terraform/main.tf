2dadfdfb0efccad556cd28d4a8960f0333d7890a
qdnqn <adnn.selimovic@gmail.com>
2024-12-19 13:16:59 +0100

    Testing

---
+++ terraform/terraform/main.tf
@@ -0,0 +1,239 @@
+provider "aws" {
+  region = local.region
+}
+
+locals {
+  name   = "apicurio-registry"
+  region = "eu-west-2"
+
+  container_registry_name = "apicurio-registry"
+  container_registry_port = "apicurio-registry"
+
+  container_ui_name = "apicurio-ui"
+  container_ui_port = "8080"
+
+  tags = {
+    Blueprint  = local.name
+    GithubRepo = "github.com/aws-ia/ecs-blueprints"
+  }
+}
+
+################################################################################
+# ECS Blueprint
+################################################################################
+
+module "ecs_service" {
+  source  = "terraform-aws-modules/ecs/aws//modules/service"
+  version = "~> 5.6"
+
+  name          = local.name
+  desired_count = 1
+  cluster_arn   = data.aws_ecs_cluster.core_infra.arn
+
+  # Task Definition
+  enable_execute_command   = true
+  requires_compatibilities = ["EC2"]
+  capacity_provider_strategy = {
+    default = {
+      capacity_provider = "apicurio-cluster" # needs to match name of capacity provider
+      weight            = 1
+      base              = 1
+    }
+  }
+
+  container_definitions = {
+    (local.container_registry_name) = {
+      image                    = "docker.io/apicurio/apicurio-registry:3.0.6"
+      readonly_root_filesystem = false
+
+      port_mappings = [
+        {
+          protocol      = "tcp",
+          containerPort = local.container_registry_port
+        }
+      ]
+
+      environment = [
+#         {
+#           name  = "",
+#           value = ""
+#         }
+      ]
+    }
+
+    (local.container_ui_name) = {
+      image                    = "docker.io/apicurio/apicurio-registry-ui:3.0.6"
+      readonly_root_filesystem = false
+
+      port_mappings = [
+        {
+          protocol      = "tcp",
+          containerPort = local.container_ui_port
+        }
+      ]
+
+      environment = [
+#         {
+#           name  = "",
+#           value = ""
+#         }
+      ]
+    }
+  }
+
+  service_registries = {
+    registry_arn = aws_service_discovery_service.this.arn
+  }
+
+  load_balancer = {
+    service = {
+      target_group_arn = module.alb.target_groups["ecs-task"].arn
+      container_name   = local.container_ui_name
+      container_port   = local.container_ui_port
+    }
+  }
+
+  subnet_ids = data.aws_subnets.private.ids
+  security_group_rules = {
+    alb_ingress = {
+      type                     = "ingress"
+      from_port                = local.container_ui_port
+      to_port                  = local.container_ui_port
+      protocol                 = "tcp"
+      description              = "Service port"
+      source_security_group_id = module.alb.security_group_id
+    }
+    egress_all = {
+      type        = "egress"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = local.tags
+}
+
+resource "aws_service_discovery_service" "this" {
+  name = local.name
+
+  dns_config {
+    namespace_id = data.aws_service_discovery_dns_namespace.this.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 9.0"
+
+  name = local.name
+
+  # For example only
+  enable_deletion_protection = false
+
+  vpc_id  = data.aws_vpc.vpc.id
+  subnets = data.aws_subnets.public.ids
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = { for subnet in data.aws_subnet.private_cidr :
+    (subnet.availability_zone) => {
+      ip_protocol = "-1"
+      cidr_ipv4   = subnet.cidr_block
+    }
+  }
+
+  listeners = {
+    http = {
+      port     = "80"
+      protocol = "HTTP"
+
+      forward = {
+        target_group_key = "ecs-task"
+      }
+    }
+  }
+
+  target_groups = {
+    ecs-task = {
+      backend_protocol = "HTTP"
+      backend_port     = local.container_ui_port
+      target_type      = "ip"
+
+      health_check = {
+        enabled             = true
+        healthy_threshold   = 5
+        interval            = 30
+        matcher             = "200-299"
+        path                = "/"
+        port                = "traffic-port"
+        protocol            = "HTTP"
+        timeout             = 5
+        unhealthy_threshold = 2
+      }
+
+      # There's nothing to attach here in this definition. Instead,
+      # ECS will attach the IPs of the tasks to this target group
+      create_attachment = false
+    }
+  }
+
+  tags = local.tags
+}
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["core-infra"]
+  }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "tag:Name"
+    values = ["core-infra-public-*"]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "tag:Name"
+    values = ["core-infra-private-*"]
+  }
+}
+
+data "aws_subnet" "private_cidr" {
+  for_each = toset(data.aws_subnets.private.ids)
+  id       = each.value
+}
+
+data "aws_ecs_cluster" "core_infra" {
+  cluster_name = "apicurio"
+}
+
+data "aws_service_discovery_dns_namespace" "this" {
+  name = "default.${data.aws_ecs_cluster.core_infra.cluster_name}.local"
+  type = "DNS_PRIVATE"
+}
