82393264c42d1b0d90b0a415e908ef039278d888
qdnqn <adnn.selimovic@gmail.com>
2024-12-19 13:18:43 +0100

    Testing

--- terraform/terraform/versions.tf
+++ terraform/terraform/versions.tf
@@ -7,4 +7,10 @@ terraform {
       version = ">= 5.0"
     }
   }
+
+  backend "s3" {
+      bucket = "$TERRAFORM_STATE_NAME"
+      region = "$AWS_REGION"
+      key    = "terraform.tfstate"
+   }
 }
